40643622e245a13fe562da0b0b04f309d356500a
bacolaws-design <bacolaws@gmail.com>
2026-02-01 13:33:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+alert("Bienvenu sur mon portfolio !");
+
+
+
+ function verifierFormulaire(){
+
+const nom = document.getElementById("nom").value;
+if (nom ===""){
+    getElementById.textCont="Veuillez ajouter votre nom !";
+    return false;
+}
+
+
+        alert("Formulaire envoyé!");
+        return true;
+
+
+}
